79bff8591cff4d12383f52930b57d673102684fe
Vladmir Upirov <vladimir.upirov@themidnightcoders.com>
2018-06-07 21:29:34 +0200

    integrate real-time database

--- src/app/persons.service.ts
+++ src/app/persons.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import Backendless from 'backendless';
 
 export class Person {
+  public objectId?: string;
   public name: string;
   public address: string;
 }
@@ -15,15 +16,41 @@ export class PersonsService {
 
   public persons: Person[] = [];
 
-  loadAll() {
+  loadAll(): void {
     PersonsStore.find<Person>().then((persons: Person[]) => {
       this.persons = persons;
+
+      this.addRealTimeListeners();
     });
   }
 
-  add(newPerson: Person) {
-    return PersonsStore.save<Person>(newPerson).then((savedPerson: Person) => {
-      this.persons.push(savedPerson);
+  add(newPerson: Person): Promise<Person> {
+    return PersonsStore.save<Person>(newPerson);
+  }
+
+  addRealTimeListeners(): void {
+    const rtHandlers: Backendless.EventHandler = PersonsStore.rt();
+
+    rtHandlers.addCreateListener<Person>(this.onPersonAdd.bind(this));
+    rtHandlers.addUpdateListener<Person>(this.onPersonChange.bind(this));
+    rtHandlers.addDeleteListener<Person>(this.onPersonRemove.bind(this));
+  }
+
+  onPersonAdd(newPerson: Person): void {
+    this.persons.push(newPerson);
+  }
+
+  onPersonChange(updatedPerson: Person): void {
+    this.persons = this.persons.map(person => {
+      return updatedPerson.objectId === person.objectId
+        ? updatedPerson
+        : person;
+    });
+  }
+
+  onPersonRemove(oldPerson: Person): void {
+    this.persons = this.persons.filter(person => {
+      return oldPerson.objectId !== person.objectId;
     });
   }
 }
